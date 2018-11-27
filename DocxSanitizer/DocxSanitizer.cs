ee4c6b12bf946f179cd8d23cde816538a4df6858
Brass <Brassw@users.noreply.github.com>
2018-11-28 01:58:23 +0200

    Initial version

---
+++ DocxSanitizer/DocxSanitizer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xceed.Words.NET;
+using Serilog;
+
+namespace DocxSanitizer
+{
+    public class DocxSanitizer
+    {
+        // Overwriting these causes problems 
+        static private string[] ignoredKeys = new string[] { "dcterms" };
+        private ILogger log;
+        public Summary Summary;
+
+        public DocxSanitizer()
+        {
+            log = Log.Logger;
+            Summary = new Summary();
+        }
+
+        public void Sanitize(IEnumerable<string> filepaths)
+        {
+            foreach(var filepath in filepaths)
+            {
+                Sanitize(filepath);
+            }
+        }
+
+        public void Sanitize(string filepath)
+        {
+            try
+            {
+                using(var document = DocX.Load(filepath))
+                {
+                    log.Debug("Processing file {filepath}", filepath);
+                    foreach(var keyVal in document.CoreProperties)
+                    {
+                        bool ignore = false;
+                        foreach(var ignoredKey in ignoredKeys)
+                        {
+                            if(keyVal.Key.Contains(ignoredKey))
+                            {
+                                ignore = true;
+                            }
+                        }
+                        if(ignore)
+                        {
+                            log.Verbose("  Ignoring core property {key}={val}.", keyVal.Key, keyVal.Value);
+                        }
+                        else {
+                            log.Verbose("  Overwriting core property {key}={val} with blank value.", keyVal.Key, keyVal.Value);
+                            document.AddCoreProperty(keyVal.Key, "");
+                        }
+                    }
+                    foreach(var keyVal in document.CustomProperties)
+                    {
+                        log.Verbose("  Removing custom property {key}={val}.", keyVal.Key, keyVal.Value);
+                        document.CustomProperties.Remove(keyVal.Key);
+                    }
+                    document.Save();
+                    Console.WriteLine("  Processed file '" + filepath + "'.");
+                    Summary.FileCount++;
+                }
+            }
+            catch(Exception e)
+            {
+                Summary.ErrorCount++;
+                Console.Error.WriteLine("  Error processing file '" + filepath + "': " + e.Message);
+                log.Debug(e.ToString());
+            }
+        }
+    }
+}
---
+++ DocxSanitizer/DocxSanitizerCLI.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Serilog;
+
+namespace DocxSanitizer
+{
+    class DocxSanitizerCLI
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration()
+                .MinimumLevel.Fatal()
+                .WriteTo.Console()
+                .CreateLogger();
+            Log.Logger = log;
+
+            if(args.Length > 0)
+            {
+                try
+                {
+                    var files = parseArgs(args);
+
+                    if(log.IsEnabled(Serilog.Events.LogEventLevel.Debug)) {
+                        log.Debug("Parsed list of files:");
+                        foreach(var file in files)
+                        {
+                            log.Debug("  " + file);
+                        }
+                    }
+
+                    Console.WriteLine("Sanitizing total " + files.Count + " files.");
+                    DocxSanitizer sanitizer = new DocxSanitizer();
+                    sanitizer.Sanitize(files);
+                    Console.Write("Finished sanitation. ");
+                    Console.WriteLine(sanitizer.Summary.formatSummary());
+                }
+                catch(Exception e)
+                {
+                    Console.Error.WriteLine(e.Message);
+                    Console.Error.WriteLine("Halting process.\n");
+                    printUsage();
+                }
+            }
+            else
+            {
+                printUsage();
+            }
+        }
+
+        static List<string> parseArgs(string[] files)
+        {
+            var list = new List<string>();
+            foreach(var file in files)
+            {
+                // Parse wildcard expressions
+                if(file.Contains("*"))
+                {
+                    int lastIndex = file.LastIndexOfAny(new char[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar });
+                    var path = file.Substring(0, lastIndex);
+                    var wildcard = file.Substring(lastIndex + 1);
+                    Log.Verbose("Split expression '{file}' into '{path}' and '{wildcard}'", file, path, wildcard);
+                    string[] globbedFiles = Directory.GetFiles(@path, wildcard);
+                    list.AddRange(globbedFiles);
+                }
+                // Use single file
+                else
+                {
+                    if(File.Exists(file))
+                    {
+                        list.Add(file);
+                    }
+                    else
+                    {
+                        throw new Exception("Unable to parse expression: " + file);
+                    }
+                }
+            }
+            return list;
+        }
+
+        static void printUsage()
+        {
+            Console.WriteLine("Docx Sanitizer removes metadata from .docx files.\n");
+            Console.WriteLine("Usage:");
+            Console.WriteLine("    DocxSanitizer [files]\n");
+            Console.WriteLine("Parameters:");
+            Console.WriteLine("    [files]    List of docx files to be sanitized, separated by spaces. Wildcard expressions, e.g. *.docx are supported.");
+        }
+    }
+}
