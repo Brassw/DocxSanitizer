790c758e9838e7f664076fb335919d2b44878ccd
Brass <Brassw@users.noreply.github.com>
2018-11-28 01:58:23 +0200

    Initial version

---
+++ DocxSanitizer/Summary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocxSanitizer
+{
+    public class Summary
+    {
+        public long FileCount = 0;
+        public long ErrorCount = 0;
+
+        public String formatSummary()
+        {
+            return "Sanitized successfully " + FileCount + " files, " + ErrorCount + " files with errors.";
+        }
+    }
+}
