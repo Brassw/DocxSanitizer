2c2093d10bddb6619cc45002a76efd9973776ff8
Brass <Brassw@users.noreply.github.com>
2018-11-29 15:24:55 +0200

    Removed Serilog dependency (smaller binary), replaced with a simple homebrew logger.

---
+++ DocxSanitizer/Logger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocxSanitizer
+{
+    class Logger
+    {
+        private bool debugEnabled = false;
+
+        public Logger()
+        {
+            try
+            {
+                var debugEnv = System.Environment.GetEnvironmentVariable("DOCX_SANITIZER_DEBUG");
+                if("true".Equals(debugEnv))
+                {
+                    debugEnabled = true;
+                }
+            }
+            catch(Exception e) { }
+        }
+
+        public void Debug(string msg, params object[] vars)
+        {
+            if(debugEnabled)
+            {
+                Console.WriteLine("DBG: " + string.Format(msg, vars));
+            }
+        }
+
+        public void Verbose(string msg, params object[] vars)
+        {
+            Debug(msg, vars);
+        }
+
+        public bool IsDebugEnabled()
+        {
+            return debugEnabled;
+        }
+    }
+}
